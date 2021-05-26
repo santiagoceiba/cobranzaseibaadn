8b925cbcdeec93430f3ba0d12148d1cc12f7152f
Santiago Hernan Duque Tamayo - Ceiba Software <santiago.duque@ceiba.com.co>
2021-05-26 08:02:32 -0500

    Creación de get,sets y constructores manuales porque no compila lombok

--- java-arquitectura-hexagonal/microservicio/aplicacion/src/main/java/com/ceiba/usuario/comando/ComandoUsuario.java
+++ java-arquitectura-hexagonal/microservicio/aplicacion/src/main/java/com/ceiba/usuario/comando/ComandoUsuario.java
@@ -2,15 +2,11 @@ package com.ceiba.usuario.comando;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ComandoUsuario{
 
     private Long id;
--- java-arquitectura-hexagonal/microservicio/dominio/src/main/java/com/ceiba/usuario/modelo/dto/DtoUsuario.java
+++ java-arquitectura-hexagonal/microservicio/dominio/src/main/java/com/ceiba/usuario/modelo/dto/DtoUsuario.java
@@ -1,12 +1,10 @@
 package com.ceiba.usuario.modelo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
+import lombok.Getter;
+
 @Getter
-@AllArgsConstructor
 public class DtoUsuario {
     private Long id;
     private String nombre;
