42dfc9d397c414b2d18040091b386bf3989e24c5
Santiago Hernan Duque Tamayo - Ceiba Software <santiago.duque@ceiba.com.co>
2021-05-25 20:06:06 -0500

    primer commit proyecto cobranzas adn ceiba

---
+++ java-arquitectura-hexagonal/microservicio/aplicacion/src/main/java/com/ceiba/usuario/comando/ComandoUsuario.java
@@ -0,0 +1,53 @@
+package com.ceiba.usuario.comando;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComandoUsuario{
+
+    private Long id;
+    private String nombre;
+    private String clave;
+    private LocalDateTime fecha;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+	public ComandoUsuario(Long id, String nombre, String clave, LocalDateTime fecha) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.clave = clave;
+		this.fecha = fecha;
+	}
+    
+    
+}
