0efc217be2ce046bcc43f026e1a86ed987e6c4b3
santiagoceiba <santiago.duque@ceiba.com.co>
2021-06-04 16:51:49 -0500

    @dirties implementacion

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/usuario/controlador/ConsultaControladorAcuerdoPagoTest.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/usuario/controlador/ConsultaControladorAcuerdoPagoTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,6 +32,7 @@ public class ConsultaControladorAcuerdoPagoTest {
     @Autowired
     private MockMvc mocMvc;
 
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     @Test
     public void listar() throws Exception{
         mocMvc.perform(get("/acuerdos/listar")
